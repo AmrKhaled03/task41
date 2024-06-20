c70d694cf138baf76ec545e15ca811bdef3b1193
Amr Khaled Marey <112765490+AmrKhaled03@users.noreply.github.com>
2024-06-21 02:44:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ const openAside = () => {
     btnOp.innerHTML = "CLOSE";
     main.style.cssText = `
       overflow: hidden;
-    width: calc(100%-300px);
+    width: calc(100%-400px);
     height: auto;
     display: flex;
     justify-content: center;
     align-items: center;
+    margin-left:400px
     `;
     document.querySelector("table").style.width = "100%";
   } else {
